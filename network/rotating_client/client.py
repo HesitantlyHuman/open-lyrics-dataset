0eb533af7ae4ba1dfbc2194f3d5d0efb666a0dd6
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2021-08-13 04:18:17 -0500

    Implemented AsyncRandomClient get and post

--- network/rotating_client/client.py
+++ network/rotating_client/client.py
@@ -1,14 +1,34 @@
 import aiohttp
 
 class AsyncRandomClient():
-    def __init__(self, proxy_file = 'network/rotating_client/proxies.json', header_file = 'network/rotating_client/headers.json'):
-        pass
+    def __init__(self, proxy_file = 'network/rotating_client/proxies.json', headers_file = 'network/rotating_client/headers.json'):
+        self.session = aiohttp.ClientSession()
 
     async def get(self, url, **kwargs):
-        pass
+        params = {
+            'headers' : self.get_headers(),
+            'proxy' : self.get_proxy(),
+            'cookies' : {}
+        }
+        params = params.update(kwargs)
+
+        return await self.session.get(url = url, **params)
 
     async def post(self, url, **kwargs):
-        pass
+        params = {
+            'headers' : self.get_headers(),
+            'proxy' : self.get_proxy(),
+            'cookies' : {}
+        }
+        params = params.update(kwargs)
+
+        return await self.session.post(url = url, **params)
 
     async def close(self):
         pass
+
+    def get_proxy(self):
+        return None
+
+    def get_headers(self):
+        return None
