ed33eb6c8b4b62e4779a36a550bc7b4be11d8996
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2021-08-13 03:00:37 -0500

    Empty class for async random client

---
+++ network/random_client.py
@@ -0,0 +1,14 @@
+import aiohttp
+
+class AsyncRandomClient():
+    def __init__(self):
+        pass
+
+    async def get(self, url, **kwargs):
+        pass
+
+    async def post(self, url, **kwargs):
+        pass
+
+    async def close(self):
+        pass
